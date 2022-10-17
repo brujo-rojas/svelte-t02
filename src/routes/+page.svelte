1185bbf128ff8a180fb29053d898164f21b297fd
Patricio Rojas R <patricio.m.rojas.r@gmail.com>
2022-10-17 16:35:42 -0300

    :package: SASS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,28 @@
-<script>
-	import Layout from "./+layout.svelte";
-</script>
-
 <layout>
-  <h1>Welcome to your library project</h1>
-  <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<h1>Welcome to your library project</h1>
+	<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-  <a href="/login">To Login</a>
+	<div class="box">
+		<div class="mini-box" />
+		<div class="mini-box" />
+		<div class="mini-box" />
+		<div class="mini-box" />
+	</div>
+
+	<a href="/login">To Login</a>
 </layout>
+
+<style lang="scss">
+	.box {
+		width: 300px;
+		height: 300px;
+		background-color: red;
+		.mini-box {
+			width: 50px;
+			height: 50px;
+			margin: 1em;
+			background-color: blue;
+		}
+	}
+</style>
