05d1e4a55cfcf354350095f26ebbb15de28f13b7
Patricio Rojas R <patricio.m.rojas.r@gmail.com>
2022-10-19 09:44:05 -0300

    :wrench: applied Prettier to Config files

--- svelte.config.js
+++ svelte.config.js
@@ -4,23 +4,23 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
-    // default options are shown
-    adapter: adapter({
-      // if true, will create a Netlify Edge Function rather
-      // than using standard Node-based functions
-      edge: false,
+		// default options are shown
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
 
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app.
-      // if `edge` is true, this option cannot be used
-      split: false
-    })
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
 	}
 };
 
