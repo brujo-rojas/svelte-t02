0402ce6a1ca433b58a644f356b7e19c07c3a6e3e
Patricio Rojas R <patricio.m.rojas.r@gmail.com>
2022-10-19 09:44:25 -0300

    :package: Header component and styles

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import HeaderSandwich from './HeaderSandwich.svelte';
+	import HeaderLogo from './HeaderLogo.svelte';
+	type HeaderItem = { name: string; href: string };
+
+	export let items: HeaderItem[] = [];
+
+	let itemSelected: HeaderItem | null = null;
+	let showMobileMenu = false;
+
+	if (items.length > 0) {
+		itemSelected = items[0];
+	}
+</script>
+
+<header>
+	<nav class="Header">
+		<div>
+			<HeaderLogo />
+			<div class="flex items-center lg:order-2">
+				<a href="/login" class="btn btn-text">Log in</a>
+				<a href="/" class="btn btn-raised--blue">Home</a>
+				<HeaderSandwich on:click={() => (showMobileMenu = !showMobileMenu)} />
+			</div>
+			<div
+				class="
+				{showMobileMenu ? '' : 'hidden'}
+				menu-container"
+				id="mobile-menu-2"
+			>
+				<ul>
+					{#each items as item}
+						<li>
+							<a
+								href={item.href}
+								class=" {item === itemSelected ? 'is-active' : ''}"
+								on:click={() => (itemSelected = item)}>{item.name}</a
+							>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
---
+++ src/components/Header/HeaderLogo.svelte
@@ -0,0 +1,8 @@
+<a href="#" class="flex items-center">
+	<img
+		src="https://kit.svelte.dev/_app/immutable/assets/stopwar-319f2582.svg"
+		class="mr-3 h-6 sm:h-9"
+		alt="Logo"
+	/>
+	<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">NAME</span>
+</a>
